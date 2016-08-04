57b11369018479e1d88a3f27a1bfb66bb2432a37
vanevery <vanevery@walking-productions.com>
2016-08-04 10:04:26 -0400

    now with retweets

--- twitter_router.js
+++ twitter_router.js
@@ -11,77 +11,82 @@ var twitter_client = new twitter({
 });
 
 function searchTweets(){
-// 	for (var h = 0; h < hashtags.length; h++) {
-		twitter_client.stream('statuses/filter', {track: keys.twitterKey.hashtag},
-			function(stream) {
-				stream.on('data', function(tweet) {    			
-					console.log("Found " + tweet.text + " from: " + tweet.user.screen_name);
-					
-					// Search hashtags first
-					for (var h = 0; h < hashtags.length; h++) {
-						if (tweet.text.indexOf(hashtags[h].hashtag) !== -1) {
-							console.log("Found " + hashtags[h].hashtag);
-							// Check users
-							for (var t = 0; t < twitterUsers.length; t++) {
-								if (tweet.user.screen_name.indexOf(twitterUsers[t].username) !== -1) {
-									console.log("Found " + tweet.user.screen_name);
-									console.log("Send SMS: " + tweet.text);
-									//sendSMS(tweet.text);
-									sendDM(tweet.text);
-									break;
-								}
+	twitter_client.stream('statuses/filter', {track: keys.twitterKey.hashtag},
+		function(stream) {
+			stream.on('data', function(tweet) {    			
+//				console.log("Found " + tweet.text + " from: " + tweet.user.screen_name);
+				
+				// Search hashtags first
+				for (var h = 0; h < keys.hashtags.length; h++) {
+					if (tweet.text.indexOf(keys.hashtags[h].hashtag) !== -1) {
+//						console.log("Found " + keys.hashtags[h].hashtag);
+						
+						// Check users
+						for (var t = 0; t < keys.twitterUsers.length; t++) {
+							if (tweet.user.screen_name.indexOf(keys.twitterUsers[t].username) !== -1) {
+//								console.log("Found " + tweet.user.screen_name);
+								send(tweet);
+								break;
 							}
-							break;
 						}
+						break;
 					}
-				});
- 
-				stream.on('error', function(error) {
-					throw error;
-				});
-			}
-		);
-// 	}
+				}
+			});
+
+			stream.on('error', function(error) {
+				console.log(error);
+			});
+		}
+	);
+}
+
+function send(tweet) {
+	//console.log(tweet);
+	retweet(tweet);
+	sendDM(tweet.text);
 }
 
 function sendDM(message) {
-	for (var i = 0; i < twitterUsers.length; i++) {
-		twitter_client.post('direct_messages/new.json', {screen_name: twitterUsers[t].username, text: message},
+	for (var i = 0; i < keys.twitterUsers.length; i++) {
+		console.log(Date.now() + " Sending: " + message + " to: " + keys.twitterUsers[i].username);
+		twitter_client.post('direct_messages/new.json', {screen_name: keys.twitterUsers[i].username, text: message},
 			function(error, tweet, response) {
-			  if(error) throw error;
-			  console.log(tweet);  // Tweet body. 
-			  console.log(response);  // Raw response object. 
+			  if (error) {
+					console.log(Date.now() + " Error Message: " + error[0].code + " " + error[0].message);
+			  } else {
+					console.log(Date.now() + " Sent To: " + tweet.recipient.screen_name + " Message: " + tweet.text);  // Tweet body. 
+			  }
+			  //console.log(response);  // Raw response object. 
 			}
 		);
 	}
 }
 
-sendDM("Hi Shawn");
-searchTweets();
+function retweet(tweet) {
+	twitter_client.post('statuses/retweet/' + tweet.id_str + '.json', {id: tweet.id_str},
+		function(error, tweet, response) {
+		  if (error) {
+				log("Error Message: " + error[0].code + " " + error[0].message);
+		  } else {
+				log("Retweeted from: " + tweet.user.screen_name + " Message: " + tweet.text);  // Tweet body. 
+		  }
+		  //console.log(response);  // Raw response object. 
+		}
+	);
+}
 
-////////////////////////
+function log(message) {
+	console.log(Date.now() + " " + message);
+}
 
-// var twilio = require('twilio');
-// var twilioNumber = keys.twilio_number;
-// var twilio_client = new twilio.RestClient(keys.twilio_account_sid, keys.twilio_auth_token);
-// 
-// function sendSMS(message) {
-// 	for (var i = 0; i < smsUsers.length; i++) {
-// 		twilio_client.sms.messages.create({
-// 			to: smsUsers[i].phonenumber,
-// 			from: twilioNumber,
-// 			body: message
-// 		}, function(error, message) {
-// 			if (!error) {
-// 				console.log("Sent " + message.sid + " on " + message.dateCreated);
-// 			} else {
-// 				console.log('Oops! There was an error:');
-// 				console.log(error);
-// 			}
-// 		});
-// 	}
-// }
-// 
+function start() {
+	log("Starting");
+	searchTweets();
+}
+
+start();
+
 
 
 
